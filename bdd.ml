f29b901bfd74b2f277c2df85abd99843a2a0e6b8
Oscar Lahaie <25438086+OscarLahaie@users.noreply.github.com>
2021-10-23 17:36:25 +0200

    Implémentation de la base de données stockant les répliques
    Ajout de différentes salutation à compléter

---
+++ bdd.ml
@@ -0,0 +1,10 @@
+let hello = [
+  "Hello ";
+  "Hello are you ready" ;
+  "Hm";
+  "Good evining";
+  "Hi are you ready";
+  "take place right now !!!";
+  "You are late !!!";
+  "I'm angry today, good luck !"
+]
