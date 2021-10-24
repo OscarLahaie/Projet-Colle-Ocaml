7d1f2823cfe82135091b56aa6ac6295a2700cd77
Oscar Lahaie <25438086+OscarLahaie@users.noreply.github.com>
2021-10-24 16:20:39 +0200

    Fonctions de décompositions
    J'ai mis en place des fonctions permettant l'analyse primaire des entrées utilisateurs. Parmis celle-ci :
    - Espace_autour qui permet d'isoler un élement
    - contient_element_caractere qui renvoie vrai si un element d'une liste de string est contenu dans une phrase
    -espace_symbols_phrase parcours la phrase pour isoler les symbols
    - decompose_phrase qui fini le processus en décomposant la phrase en une liste de mots et de ponctuations

--- Colleur.ml
+++ Colleur.ml
@@ -1,4 +1,7 @@
-let ponctuation = [' '; '\n'; '.'; ','; ';'; '-'; '!'; '?'];;
+(* Début des fonctions de décompositions *)
+
+let ponctuation_char = [' '; '\n'; '.'; ','; ';'; '-'; '!'; '?'];;
+let ponctuation_string = [" "; "\n"; "."; ","; ";"; "-"; "!"; "?"];;
 
 let rec contient_element_caractere phrase liste = 
   match liste with
@@ -7,19 +10,28 @@ let rec contient_element_caractere phrase liste =
   | _ :: q -> contient_element_caractere phrase q 
 ;;
 
-let separateur_de_phrase phrase element =
-  let rec separateur_aux n =
-  match phrase with
-  | "" -> ""
-  | phrase when (String.sub phrase n  1) = element  -> (String.sub phrase 0 n) ^ " " ^ element ^ " " ^(String.sub phrase (n+1) (String.length phrase - n - 1))
-  | phrase -> separateur_aux (n + 1)
-  in separateur_aux 0
-;;
+let rec espace_autour partie_de_phrase liste =
+  match liste with
+  | [] -> partie_de_phrase
+  | t :: q when t = partie_de_phrase -> " " ^ t ^ " "
+  | _ :: q -> espace_autour partie_de_phrase q
 ;;
 
-let rec diviseur_de_phrase phrase =
-  let liste_de_mots = String.split_on_char ' ' phrase in 
-  let rec extraction_de_symbole mot_en_liste =
-    match mot_en_liste with
+let espace_symbols_phrase phrase =
+  let rec iter phrase2 =
+    match (String.sub phrase2 0 1) with
+    | partie when String.length phrase2 = 1 -> (espace_autour partie ponctuation_string)
+    | partie  -> (espace_autour partie ponctuation_string) ^  iter (String.sub phrase2 1 (String.length phrase2 - 1)) 
+  in iter phrase 
+;;
+
+let decompose_phrase phrase =
+  let rec decompose_aux phrase2 = 
+    match phrase2 with
     | [] -> []
-    | t :: q when contient_element_caractere t ponctuation -> 
+    | t :: q when t = "" -> decompose_aux q
+    | t :: q -> t :: decompose_aux q
+  in decompose_aux (String.split_on_char ' ' (espace_symbols_phrase phrase))
+;;
+
+(*Fin des fonctions de décompositions*)
