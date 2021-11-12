660057104f8899f7c068a42869d51f90510b1ee2
Oscar Lahaie <oscarlahaie83@gmail.com>
2021-11-12 20:25:13 +0100

    bdd update

--- bdd.ml
+++ bdd.ml
@@ -7,6 +7,45 @@ let hello = [
   "take place right now !!!";
   "You are late !!!";
   "I'm angry today, good luck !" ; 
-  "Hi my name is Adrien ";
-  "salut" 
+  "Quick we are already late !"
+];;
+
+let demande_nom = [
+  "What is your name ?";
+  "Tell me your name please";
+  "How can i call you ?";
+  "Hmm you are ?"
+]
+
+let punchline = [
+  "You are really really bad !";
+  "Even the PCSI are better ! No excuse me even the PTSI are better";
+  "I know why the Maths level is so low in France it is because of you";
+  "Are you really coming during the lessons";
+  "Why are you here ! You should be in a Factory !"
+]
+
+let good = [
+  "It is perfect !";
+  "Very very good !";
+  "Perfect !";
+  "Okay nice !";
+  "It is totally accurate !"
+]
+
+let medium = [
+  "It is okay !";
+  "Yes kind of ...";
+  "Okay";
+  "Maybe ...";
+  "Not that bad !"
+]
+
+let not_good = [
+  "It is false !";
+  "How can you say that !";
+  "You have to focus on the subject";
+  "Do you really understand the question ?";
+  "Not great ...";
+  "You have to learn you lesson"
 ]
